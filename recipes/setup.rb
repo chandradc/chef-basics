e251c4f56e148287913ed441391b50203b8f3b64
vagrant <vagrant@localhost.localdomain>
2020-07-10 07:48:09 +0000

    first commit

---
+++ recipes/setup.rb
@@ -0,0 +1,21 @@
+package 'tree' do
+  action :install
+end
+
+package 'ntp' do
+  action [ :install ]
+end
+
+service 'ntpd' do
+  action [ :enable, :start ]
+end
+
+
+file '/etc/motd' do
+  content 'This server is the property of Chandra'
+  mode '0777'
+  owner 'root'
+  group 'root'
+end
+
+package 'git'
